c89e9a142777b561c807f0419968c01a82122e37
fredrikkri <fjkri@hotmail.com>
2024-09-21 16:15:53 +0200

    laget mer enkelt ui

--- app/src/main/java/com/example/mobprog/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mobprog/auth/LoginActivity.kt
@@ -5,21 +5,33 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun LoginActivity(modifier: Modifier = Modifier) {
+
+    var usernameTextController by remember {
+        mutableStateOf("")
+    }
+
+    var passwordTextController by remember {
+        mutableStateOf("")
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -28,21 +40,25 @@ fun LoginActivity(modifier: Modifier = Modifier) {
     ) {
         Text(text = "Welcome to Arena", fontSize = 28.sp, fontWeight = FontWeight.Bold)
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(28.dp))
 
         Text(text = "Enter username and password", fontSize = 16.sp)
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        OutlinedTextField(value = "", onValueChange = {}, label = {
+        OutlinedTextField(value = usernameTextController, onValueChange = {
+            usernameTextController = it
+        }, label = {
             Text(text = "Username")
         })
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        OutlinedTextField(value = "", onValueChange = {}, label = {
+        OutlinedTextField(value = passwordTextController, onValueChange = {
+            passwordTextController = it
+        }, label = {
             Text(text = "Password")
-        })
+        }, visualTransformation = PasswordVisualTransformation())
 
         Spacer(modifier = Modifier.height(20.dp))
 
@@ -52,7 +68,7 @@ fun LoginActivity(modifier: Modifier = Modifier) {
 
         Spacer(modifier = Modifier.height(10.dp))
 
-        TextButton(onClick = { /*TODO*/ }) {
+        TextButton(onClick = {  }) {
             Text(text = "Don't have an account? Click to register")
         }
     }
--- app/src/main/java/com/example/mobprog/auth/RegisterActivity.kt
+++ app/src/main/java/com/example/mobprog/auth/RegisterActivity.kt
@@ -10,9 +10,14 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -20,28 +25,40 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun RegisterActivity(modifier: Modifier = Modifier) {
 
+    var usernameTextController by remember {
+        mutableStateOf("")
+    }
+
+    var passwordTextController by remember {
+        mutableStateOf("")
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Register Page", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+        Text(text = "Register user", fontSize = 28.sp, fontWeight = FontWeight.Bold)
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(28.dp))
 
         Text(text = "Enter username and password", fontSize = 16.sp)
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        OutlinedTextField(value = "", onValueChange = {}, label = {
+        OutlinedTextField(value = usernameTextController, onValueChange = {
+            usernameTextController = it
+        }, label = {
             Text(text = "Username")
         })
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        OutlinedTextField(value = "", onValueChange = {}, label = {
+        OutlinedTextField(value = passwordTextController, onValueChange = {
+            passwordTextController = it
+        }, label = {
             Text(text = "Password")
-        })
+        }, visualTransformation = PasswordVisualTransformation())
 
         Spacer(modifier = Modifier.height(20.dp))
 
