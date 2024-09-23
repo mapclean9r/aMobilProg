e5c7f6bc7600fb5d4f950d5a02eb187311ee2371
mapclean9r <tobias.the@hotmail.no>
2024-09-23 13:11:48 +0200

    added more base

--- app/src/main/java/com/example/mobprog/guild/Guild.kt
+++ app/src/main/java/com/example/mobprog/guild/Guild.kt
@@ -6,10 +6,10 @@ class Guild (){
     private lateinit var picture: String;
 
     // change type to User
-    private lateinit var guildMembers: List<String>;
+    private var guildMembers: ArrayList<String> = ArrayList();
 
     //change type to Events
-    private lateinit var guildEvent: List<String>;
+    private var guildEvents: ArrayList<String> = ArrayList();
 
     constructor(guildName: String, guildDescription: String, guildPicture: String) : this(){
         name = guildName;
@@ -19,20 +19,44 @@ class Guild (){
 
     // add param User
     fun addGuildMember(){
-
-    }
-
-    // add param User
-    fun joinGuild(){
-
+        guildMembers.add("e")
     }
 
     // add param event
     fun addEvent(){
-
+        guildEvents.add("")
     }
 
+    fun setPicture(newPicture: String){
+        picture =  newPicture;
+    }
 
+    fun setDescription(newDescription: String){
+        description = newDescription;
+    }
 
+    fun setGuildName(newGuildName: String){
+        description = newGuildName;
+    }
+
+    fun getGuildName(): String {
+        return name;
+    }
+
+    fun getGuildDescription(): String {
+        return description;
+    }
+
+    fun getGuildPicture(): String {
+        return picture;
+    }
+
+    fun getEvents(): ArrayList<String> {
+        return guildEvents;
+    }
+
+    fun getMembers(): ArrayList<String> {
+        return guildMembers;
+    }
 
 }
