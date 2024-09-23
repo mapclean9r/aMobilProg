6c1752a499ce5455296c7b007d4ea224c8d95b79
Aksellllllll <aksonator@gmail.com>
2024-09-23 13:18:36 +0200

    TODO: Database

--- app/src/main/java/com/example/mobprog/user/User.kt
+++ app/src/main/java/com/example/mobprog/user/User.kt
@@ -1,17 +1,19 @@
 package com.example.mobprog.user
 
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
 
-class User(private var id: Int = -1, val name: String, var password: String) {
+class User(private var id: String = "", val name: String, var password: String) {
 
     constructor(name: String, password: String
-    ): this(-1, name, password) {
+    ): this("", name, password) {
 
     }
 
     init {
-        if(id < 0) { // Check if user exists in db.
-            //TODO get user from db
+        if (id.isBlank()) {
+            // TODO get user from name and password, throw exception if not found(?)
         }
     }
 
