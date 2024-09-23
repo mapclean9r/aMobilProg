991f037c735511193d3c9f5a89f0f0288a40309f
fredrikkri <fjkri@hotmail.com>
2024-09-23 14:32:10 +0200

    prøver å få til metode for å hente en enkelt bruker

--- app/src/main/java/com/example/mobprog/data/UserService.kt
+++ app/src/main/java/com/example/mobprog/data/UserService.kt
@@ -1,31 +1,39 @@
 package com.example.mobprog.data
 
 import android.util.Log
-import com.example.mobprog.user.User
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.toObject
+import kotlinx.coroutines.tasks.await
 
-fun printAllUsers() {
-    val db: FirebaseFirestore = FirebaseFirestore.getInstance()
-    val allUsersRef = db.collection("users")
+    fun printAllUsers() {
+        val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+        val allUsersRef = db.collection("users")
 
-    allUsersRef
-        .get()
-        .addOnSuccessListener { users ->
-            for (user in users) {
-                println("Userid: ${user.id}, Username: ${user.get("username")}, Password: ${user.get("password")}",)
+        allUsersRef
+            .get()
+            .addOnSuccessListener { users ->
+                for (user in users) {
+                    println(
+                        "Userid: ${user.id}, Username: ${user.get("username")}, Password: ${
+                            user.get(
+                                "password"
+                            )
+                        }",
+                    )
+                }
             }
-        }
-        .addOnFailureListener { exception ->
-            Log.w("Error getting documents: ", exception)
-        }
-}
-
-fun getUserWithId(id: String): Unit {
-    val db: FirebaseFirestore = FirebaseFirestore.getInstance()
-    val userRef = db.collection("users").document(id)
-
-    userRef.get().addOnSuccessListener { documentSnapshot ->
-        val user = documentSnapshot.toObject<User>();
+            .addOnFailureListener { exception ->
+                Log.w("Error getting documents: ", exception)
+            }
     }
-}
+
+    suspend fun getUserWithId(id: String): DocumentSnapshot? {
+        val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+        return try {
+            val usersCollection = db.collection("users");
+            usersCollection.document(id).get().await()
+        } catch (e: Exception) {
+            println("The user dosent exist.")
+            return null;
+        }
+    }
