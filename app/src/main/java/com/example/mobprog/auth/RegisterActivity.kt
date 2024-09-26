64f744dc94e27853970c7b762acf2a28ff15219e
fredrikkri <fjkri@hotmail.com>
2024-09-26 13:48:57 +0200

    fiksa litt små greier

--- app/src/main/java/com/example/mobprog/auth/RegisterActivity.kt
+++ app/src/main/java/com/example/mobprog/auth/RegisterActivity.kt
@@ -23,18 +23,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.example.mobprog.createEvent.EventBase
-import com.example.mobprog.data.GuildService
 import com.example.mobprog.data.UserService
-import com.example.mobprog.guild.Guild
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
 
 @Composable
-fun RegisterActivity(navController: NavController, modifier: Modifier = Modifier) {
-    val userService = UserService();
-    val auth = Firebase.auth;
+fun RegisterActivity(navController: NavController) {
+    val userService = UserService()
 
     var emailTextController by remember {
         mutableStateOf("")
--- app/src/main/java/com/example/mobprog/data/UserService.kt
+++ app/src/main/java/com/example/mobprog/data/UserService.kt
@@ -5,18 +5,11 @@ import com.example.mobprog.guild.Guild
 import com.example.mobprog.user.User
 import com.google.android.gms.tasks.Task
 import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.firestore
 
 class UserService {
     private val db = Firebase.firestore
-    private lateinit var userName: String
-    val auth = Firebase.auth
-
-    fun getUsername(user: String) : String {
-        return "e"
-    }
 
     fun printAllUsers() {
         val allUsersRef = db.collection("users")
@@ -47,10 +40,10 @@ class UserService {
 
     fun getUserWithId(id: String): Task<DocumentSnapshot> {
         val userRef = db.collection("users").document(id).get()
-        return userRef;
+        return userRef
     }
 
-    fun getUserByEmail(inputEmail: String) {
+    private fun getUserByEmail(inputEmail: String) {
         db.collection("users")
             .whereEqualTo("email", inputEmail)
             .get()
@@ -71,11 +64,6 @@ class UserService {
                 }
                 callback(emailField)
             }
-    }
-
-
-    fun loginAuthCheck(inputEmail: String, inputPassword: String) {
-        var existingUser = getUserByEmail(inputEmail)
     }
 
     fun sendData(guild: Guild) {
