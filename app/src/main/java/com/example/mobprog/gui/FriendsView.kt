adef7c6c897d0a8e92aceac42926d5240b48c1c8
Aksellllllll <aksonator@gmail.com>
2024-09-27 13:44:57 +0200

    oppdatert friends view

--- app/src/main/java/com/example/mobprog/gui/FriendsView.kt
+++ app/src/main/java/com/example/mobprog/gui/FriendsView.kt
@@ -1,13 +1,183 @@
 package com.example.mobprog.gui
 
+import android.annotation.SuppressLint
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.mobprog.R
+import com.example.mobprog.data.UserService
+import com.example.mobprog.gui.components.BottomNavBar
+import com.example.mobprog.user.User
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun FriendsActivity(navController: NavController) {
 
+
+    Scaffold(topBar = {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(88.dp)
+                .padding(bottom = 10.dp, top = 24.dp)
+                .background(MaterialTheme.colorScheme.primary),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconButton(onClick = {
+                UserService().getCurrentUserData { docFields ->
+                    println("$docFields")
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon",
+                    tint = Color.White
+                )
+            }
+            Text(
+                text = "Friends",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            IconButton(onClick = { navController.navigate("profileScreen") }) {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Profile Icon",
+                    tint = Color.White
+                )
+            }
+        }
+    }, content = {
+        FriendsList()
+    }, bottomBar = {
+            // inspirert av link under for å lage navbar.
+            // https://www.youtube.com/watch?v=O9csfKW3dZ4
+            BottomNavBar(navController = navController)
+        })
+}
+
+@Composable
+fun FriendsList() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(28.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val friends = ArrayList<User>()
+
+        friends.add(User("Aksel", "Aksel", "123"))
+        friends.add(User("Fredrik", "Fredrik", "123"))
+        friends.add(User("Tobias", "Tobias", "123"))
+        friends.add(User("Ole", "Ole", "123"))
+
+
+        val selectedFriend = remember { mutableStateOf<User?>(null) }
+
+        BackHandler(selectedFriend.value != null) {
+            selectedFriend.value = null
+        }
+
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Surface(
+            color = MaterialTheme.colorScheme.background
+        ) {
+            LazyColumn {
+                items(friends) { friend ->
+                    FriendItem(friend) {
+                        selectedFriend.value = friend
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun FriendItem(friend: User, onClick: () -> Unit) {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(8.dp)
+            //.border(2.dp, Color.LightGray, shape = RoundedCornerShape(10.dp))
+        ,
+        verticalAlignment = Alignment.CenterVertically
+
+    ){
+
+            Image(
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(56.dp),
+                painter = painterResource(id = R.drawable.lol),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Column(
+                modifier = Modifier.padding(start = 16.dp)
+            ) {
+                Text(
+                    text = friend.getUsername(),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    style = TextStyle(fontFamily = FontFamily.SansSerif, fontSize = 18.sp, color = Color.Black)
+                )
+            }
+    }
 }
 
 @Preview(showBackground = true)
