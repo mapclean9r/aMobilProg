fba69efe8dd2b5b24f245525ac0ca7f3206f49fd
mapclean9r <tobias.the@hotmail.no>
2024-09-27 13:07:36 +0200

    ;(

--- app/src/main/java/com/example/mobprog/data/GuildService.kt
+++ app/src/main/java/com/example/mobprog/data/GuildService.kt
@@ -1,18 +1,34 @@
 package com.example.mobprog.data
 
 import com.example.mobprog.createEvent.EventBase
+import com.example.mobprog.createEvent.EventManager
 import com.example.mobprog.guild.Guild
-import com.example.mobprog.user.User
 import com.google.firebase.Firebase
-import com.google.firebase.firestore.Filter
-import com.google.firebase.firestore.QuerySnapshot
 import com.google.firebase.firestore.firestore
+import kotlin.collections.ArrayList
 
 class GuildService {
     private val db = Firebase.firestore
 
     fun registerEventsToGuild(guild: Guild) {
+        var events = db.collection("events")
+        events.document("Friends of Legends").set(EventBase("Friends of Legends", 23, "small boy"))
         db.collection("guilds").document(guild.getGuildName()).set(guild)
+    }
+
+    fun getEventRef(guildEvents: EventManager): ArrayList<String>{
+        var guildRef: ArrayList<String> = ArrayList()
+
+        var events = db.collection("events").document().id
+
+        for (e in guildEvents.getEvents()){
+            for (i in events){
+                if (e.getName() == i.toString()){
+                    guildRef.add(i.toString())
+                }
+            }
+        }
+        return guildRef
     }
 
     fun registerEvent(event: EventBase){
