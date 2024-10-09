e5c42f61f7faa2ecb2f1131fb8363989ff95371c
evjengit <ole_fram.96@hotmail.com>
2024-10-09 13:49:01 +0200

    Major User Update

--- app/src/main/java/com/example/mobprog/gui/HomeView.kt
+++ app/src/main/java/com/example/mobprog/gui/HomeView.kt
@@ -49,22 +49,9 @@ import com.google.firebase.firestore.FirebaseFirestore
 fun HomeView(navController: NavController, eventService: EventService, modifier: Modifier = Modifier) {
 
     var events by remember { mutableStateOf(emptyList<EventData>()) }
-
     var showSearch by remember { mutableStateOf(false) }
     var searchText by remember { mutableStateOf("") }
-
     var filteredEvents by remember { mutableStateOf(emptyList<EventData>()) }
-
-    val currentUser = FirebaseAuth.getInstance().currentUser
-    if (currentUser != null) {
-        val uid = currentUser.uid
-        val db = FirebaseFirestore.getInstance()
-        val userRef = db.collection("users").document(uid)
-        println("uid: $uid")
-        println("db: $db")
-        println("userRef: $userRef")
-        println("currentUser: $currentUser")
-    }
 
     eventService.getAllEvents { eventsList ->
         eventsList?.let { documents ->
@@ -124,12 +111,12 @@ fun HomeView(navController: NavController, eventService: EventService, modifier:
             ) {
                 if(filteredEvents.isEmpty()){
                     items(events) { event ->
-                        EventBox(eventData = event)
+                        EventBox(eventData = event, navController)
                         Spacer(modifier = Modifier.height(16.dp))
                     }   
                 }
                 items(filteredEvents) { event ->
-                    EventBox(eventData = event)
+                    EventBox(eventData = event, navController)
                 }
             }
         },
--- app/src/main/java/com/example/mobprog/user/UserData.kt
+++ app/src/main/java/com/example/mobprog/user/UserData.kt
@@ -1,17 +1,21 @@
 package com.example.mobprog.user
 
-import java.util.UUID
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-data class UserData (
+val calendar: Calendar = Calendar.getInstance()
+@SuppressLint("ConstantLocale")
+val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+val formattedDateTime: String = dateFormat.format(calendar.time)
+
+data class UserData(
+    val id: String = "",
     val name: String = "",
-    val password: String = "",
     val email: String = "",
     val picture: String = "",
-    val creatorId: String = generateId(),
     val friends: List<String> = emptyList(),
-    val guild: String = ""
+    val guild: String = "",
+    val dateCreated: String = formattedDateTime
 )
-
-fun generateId(): String {
-    return UUID.randomUUID().toString()
-}
