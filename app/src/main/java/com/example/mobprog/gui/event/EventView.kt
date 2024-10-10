f03d796faf75329025232e71ba962f96f1b40e01
fredrikkri <fjkri@hotmail.com>
2024-10-10 10:20:16 +0200

    fjerna ubrukte imports

--- app/src/main/java/com/example/mobprog/gui/event/EventView.kt
+++ app/src/main/java/com/example/mobprog/gui/event/EventView.kt
@@ -1,26 +1,16 @@
 package com.example.mobprog.gui.event
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,16 +20,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
-import com.example.mobprog.api.GameData
 import com.example.mobprog.createEvent.EventData
-import com.example.mobprog.data.EventService
-import com.example.mobprog.data.UserService
-import com.example.mobprog.gui.components.BottomNavBar
-import com.example.mobprog.gui.components.EventBox
 import com.google.gson.Gson
 
 @Composable
