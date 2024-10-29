277f5ed1c6ea863fb1df15bcbbddb8e23a01ea34
mapclean9r <tobias.the@hotmail.no>
2024-10-29 12:29:33 +0100

    big fix - Guild view shows pic selected

--- app/src/main/java/com/example/mobprog/gui/components/GuildProfilePicture.kt
+++ app/src/main/java/com/example/mobprog/gui/components/GuildProfilePicture.kt
@@ -1,13 +1,21 @@
 package com.example.mobprog.gui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
@@ -16,7 +24,7 @@ import com.example.mobprog.data.handlers.ImageHandler
 private val imgHandler = ImageHandler()
 
 @Composable
-fun GetSelfProfileImage() {
+fun GetGuildProfileImage() {
     var imageUrl by remember { mutableStateOf<String?>(null) }
     var error by remember { mutableStateOf<String?>(null) }
 
@@ -30,7 +38,7 @@ fun GetSelfProfileImage() {
     if (imageUrl != null) {
         Image(
             painter = rememberAsyncImagePainter(imageUrl),
-            contentDescription = "User Profile Image",
+            contentDescription = "Guild Profile Image",
             modifier = Modifier,
             contentScale = ContentScale.Crop
         )
@@ -42,12 +50,13 @@ fun GetSelfProfileImage() {
 }
 
 @Composable
-fun GetSelfProfileImageCircle(size: Int) {
+fun GetGuildProfileImageCircle(guildID: String, size: Int) {
     var imageUrl by remember { mutableStateOf<String?>(null) }
     var error by remember { mutableStateOf<String?>(null) }
 
     LaunchedEffect(Unit) {
-        imgHandler.getUserProfileImageUrl(
+        imgHandler.getGuildImageUrl(
+            guildID = guildID,
             onSuccess = { url -> imageUrl = url },
             onFailure = { exception -> error = exception.message }
         )
@@ -61,6 +70,36 @@ fun GetSelfProfileImageCircle(size: Int) {
                 .size(size.dp)
                 .clip(CircleShape),
             contentScale = ContentScale.Crop
+        )
+    } else if (error != null) {
+        Text(text = "Error loading image: $error")
+    } else {
+        Text("Loading...")
+    }
+}
+
+@Composable
+fun GetGuildProfileImageView(guildID: String, size: Int) {
+    var imageUrl by remember { mutableStateOf<String?>(null) }
+    var error by remember { mutableStateOf<String?>(null) }
+
+    LaunchedEffect(Unit) {
+        imgHandler.getGuildImageUrl(
+            guildID = guildID,
+            onSuccess = { url -> imageUrl = url },
+            onFailure = { exception -> error = exception.message }
+        )
+    }
+    var widthSize = size/2
+    if (imageUrl != null) {
+        Image(
+            painter = rememberAsyncImagePainter(imageUrl),
+            contentDescription = "User Profile Image",
+            modifier = Modifier
+                .height(widthSize.dp)
+                .width(size.dp)
+                .clip(RectangleShape),
+            contentScale = ContentScale.FillWidth
         )
     } else if (error != null) {
         Text(text = "Error loading image: $error")
