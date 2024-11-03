a10dc06fcaf8bac28cdcfcd64bbb000d827dc8c1
evjengit <ole_fram.96@hotmail.com>
2024-11-03 11:10:45 +0100

    Android Studio update
    Byttet android gradle plugin versjon til 8.7.2 slik at nyeste features er tilgjengelig. Må ha Android Studio Ladybug | 2024.2.1 Patch 2. La til en import NavType som manglet i mainactivity.

--- app/src/main/java/com/example/mobprog/MainActivity.kt
+++ app/src/main/java/com/example/mobprog/MainActivity.kt
@@ -59,6 +59,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
 import com.example.mobprog.gui.event.eventDataSaver
 
 @SuppressLint("StaticFieldLeak")
